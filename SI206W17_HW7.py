9609409750844634a21b1e850118e8f0808a0de7
Sam Karmin <skarmin@umich.edu>
2017-03-26 22:27:33 -0400

    Final

--- SI206W17_HW7.py
+++ SI206W17_HW7.py
@@ -114,14 +114,60 @@ for tweet in umsi_tweets:
 
 # Use the database connection to commit the changes to the database
 conn.commit()
-    
+
 
 
 # You can check out whether it worked in the SQLite browser! (And with the tests.)
 
 
 
+## [PART 2] - SQL statements
 
+## In this part of the homework, you will write a number of Python/SQL statements to get data from the database, as directed. For each direction, write Python code that includes an SQL statement that will get the data from your database. 
+## You can verify whether your SQL statements work correctly in the SQLite browser! (And with the tests)
+
+
+# Select from the database all of the TIMES the tweets you collected were posted and fetch all the tuples that contain them in to the variable tweet_posted_times.
+statement = 'SELECT time_posted FROM Tweets'
+result = cur.execute(statement)
+tweet_posted_times = result.fetchall()
+
+# Select all of the tweets (the full rows/tuples of information) that have been retweeted MORE than 2 times, and fetch them into the variable more_than_2_rts.
+statement = 'SELECT * FROM Tweets WHERE retweets > 2'
+result = cur.execute(statement)
+more_than_2_rts = result.fetchall()
+
+
+
+# Select all of the TEXT values of the tweets that are retweets of another account (i.e. have "RT" at the beginning of the tweet text). Save the FIRST ONE from that group of text values in the variable first_rt. Note that first_rt should contain a single string value, not a tuple.
+statement = "SELECT tweet_text FROM Tweets WHERE instr(tweet_text, 'RT')"
+result = cur.execute(statement)
+first_rt = result.fetchone()[0]
+
+
+# Finally, done with database stuff for a bit: write a line of code to close the cursor to the database.
+
+conn.close()
+
+
+## [PART 3] - Processing data
+
+# Define a function get_twitter_users that accepts a string as in put and returns a SET of the _twitter screennames_ of each twitter user who was mentioned in that string. 
+
+# Note that the syntax for mentions in a tweet is that the username is preceded by an "@" character, e.g. "@umsi" or "@aadl", and cannot contain any punctuation besides underscores -- that's how to determine what user names are mentioned. (e.g. @hello? is just the username "hello", but @programmer_at_umsi is "programmer_at_umsi"). 
+
+#re.match and getting the 0th group from the MatchObject may be useful for you here... reminder: http://stackoverflow.com/questions/15340582/python-extract-pattern-matches
+
+# You may assume for this problem that there will be no usernames directly in order, e.g. "@hello@goodbye", only "@hello and @goodbye" for example. We will iterate on this later!
+
+# Also note that the SET type is what this function should return, NOT a list or tuple. We looked at very briefly at sets when we looked at set comprehensions last week. In a Python 3 set, which is a special data type, it's a lot like a combination of a list and a dictionary: no key-value pairs, BUT each element in a set is by definition unique. You can't have duplicates.
+
+# If you want to challenge yourself here -- this function definition (what goes under the def statement) CAN be written in one line! Definitely, definitely fine to write it with multiple lines, too, which will be much easier and clearer.
+
+
+def get_twitter_users(s):
+	exp = '@([A-Za-z0-9_]*)'
+	return set(re.findall(exp,s))
 
 
 
